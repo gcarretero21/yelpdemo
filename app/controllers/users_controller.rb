517e9ca550e3dff58062213eae060584c90d50e7
Gabriela <gcarretero_2021@depauw.edu>
2021-05-18 10:40:04 -0400

    the code

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+ 
+  def reviews
+    @user = User.find(params[@id])
+    @reviews = @user.reviews
+  end
+
+
+end
